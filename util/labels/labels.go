949d5ca58f8dbbee3eaf9028d68bd419b2cd4d35
Waleed Malik <ahmedwaleedmalik@gmail.com>
2020-11-20 09:56:26 +0500

    Add comments to labels.go

--- util/labels/labels.go
+++ util/labels/labels.go
@@ -2,14 +2,17 @@ package labels
 
 // AddLabel inserts/updates a label in an existing map of labels
 func AddLabel(labels map[string]string, key string, value string) map[string]string {
+	// If key is empty, no changes are required
 	if key == "" {
 		return labels
 	}
 
+	// Initialize an empty map if it's nill
 	if labels == nil {
 		labels = make(map[string]string)
 	}
 
+	// Add label
 	labels[key] = value
 	return labels
 }
